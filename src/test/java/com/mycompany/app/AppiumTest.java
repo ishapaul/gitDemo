c2b53c8d279224ade0e04a9535c2d686a2cc7457
ishapaul <ishapaul20@gmail.com>
2019-08-30 01:34:05 +0530

    first commit

---
+++ src/test/java/com/mycompany/app/AppiumTest.java
@@ -0,0 +1,14 @@
+package com.mycompany.app;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	@Test
+	public void mobileAutomation()
+	{
+		System.out.println("Appium");
+	}
+
+
+}
